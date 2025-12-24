ea0ae214dfba575bfb29b8fda4036048a3aa0e05
IshankAggarwal09 <aggarwalishu263@gmail.com>
2025-12-24 19:14:25 +0530

    Time: 2 ms (74.32%), Space: 55.6 MB (38.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -8,7 +8,7 @@ class Solution {
                 count = 1;
                 el = v[i];
             }
-            else if (el == v[i]) {
+            else if(el == v[i]) {
                 count++;
             }
             else {
