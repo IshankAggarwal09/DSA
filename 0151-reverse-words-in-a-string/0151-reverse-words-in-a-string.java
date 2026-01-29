bec7a5b5a6f6c9f7611c604c0b01b032b4ad339a
IshankAggarwal09 <aggarwalishu263@gmail.com>
2026-01-29 19:25:09 +0530

    Time: 9 ms (23.93%), Space: 44 MB (90.17%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,49 +1,20 @@
-public class Solution {
+class Solution {
     public String reverseWords(String s) {
-        if (s == null) { 
-            return null;
-        }
-        char[] a = s.toCharArray();
-        int n = a.length;
-        reverse(a, 0, n - 1);
-        reverseWords(a, n);
-        return cleanSpaces(a, n);
-    }
-    void reverseWords(char[] a, int n) {
-        int i = 0, j = 0;
-        while (i < n) {
-            while (i < j || i < n && a[i] == ' ') { 
-                i++;
-            } 
-            while (j < i || j < n && a[j] != ' ') { 
-                j++;
-            } 
-            reverse(a, i, j - 1);                      
-        }
-    }
-    String cleanSpaces(char[] a, int n) {
-        int i = 0, j = 0;  
-        while (j < n) {
-            while (j < n && a[j] == ' ') { 
-                j++;
-            }             
-            while (j < n && a[j] != ' ') { 
-                a[i++] = a[j++];
+        ArrayList<String> words = new ArrayList<>();
+        StringBuilder word = new StringBuilder();
+        for (int i=0; i<s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                word.append(s.charAt(i));
             }
-            while (j < n && a[j] == ' ') { 
-                j++;
-            }            
-            if (j < n) {
-                a[i++] = ' ';
-            }                     
+            else if (word.length() > 0) {
+                words.add(word.toString());
+                word.setLength(0);
+            }
         }
-        return new String(a).substring(0, i);
-    }
-    private void reverse(char[] a, int i, int j) {
-        while (i < j) {
-            char t = a[i];
-            a[i++] = a[j];
-            a[j--] = t;
+        if (word.length() > 0) {
+            words.add(word.toString());
         }
+        Collections.reverse(words);
+        return String.join(" ", words);
     }
 }
