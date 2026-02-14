249eca686f98eb3d5c2c782196b7b12fd530e1a4
IshankAggarwal09 <aggarwalishu263@gmail.com>
2026-02-15 01:41:45 +0530

    Time: 3 ms (87.48%), Space: 43.6 MB (15.98%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,8 +1,5 @@
 class Solution {
     public boolean isValid(String s) {
-        if (s.length() % 2 != 0) {
-            return false;
-        }
         Stack<Character> st = new Stack<>();
         for (char ch : s.toCharArray()) {
             if (ch == '(' || ch == '[' || ch == '{') {
