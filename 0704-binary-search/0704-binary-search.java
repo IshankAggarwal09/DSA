9c0d7a9beae5af41e28e2026ea9d0bfd84193158
IshankAggarwal09 <aggarwalishu263@gmail.com>
2026-05-17 01:05:44 +0530

    Time: 0 ms (100%), Space: 47.8 MB (95.94%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -3,15 +3,15 @@ class Solution {
         int start = 0;
         int end = nums.length-1;
         while (start <= end) {
-            int mid = start + (end - start) / 2;
+            int mid = start + (end-start)/2;
             if (nums[mid] == target) {
                 return mid;
             }
-            else if (nums[mid] > target) {
-                end = mid - 1;
+            else if (nums[mid] < target) {
+                start = mid + 1;
             }
             else {
-                start = mid + 1;
+                end = mid - 1;
             }
         }
         return -1;
