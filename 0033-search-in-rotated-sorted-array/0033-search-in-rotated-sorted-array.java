1437c2f03c4523198f7e010cc85051a9f4526218
IshankAggarwal09 <aggarwalishu263@gmail.com>
2026-05-26 16:35:05 +0530

    Time: 0 ms (100%), Space: 43.7 MB (64.14%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -14,7 +14,7 @@ class Solution {
                 else {
                     start = mid + 1;
                 }
-            }
+            } 
             else {
                 if (target > nums[mid] && target <= nums[end]) {
                     start = mid + 1;
