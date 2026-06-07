28c9ac83688033364bbed5426aed50be34674ab6
IshankAggarwal09 <aggarwalishu263@gmail.com>
2026-06-07 22:18:04 +0530

    Time: 0 ms (100%), Space: 43.1 MB (81.47%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -6,7 +6,7 @@ class Solution {
         String prefix = strs[0];
         for (int i=0; i<strs.length; i++) {
             while (!strs[i].startsWith(prefix)) {
-                prefix = prefix.substring(0, prefix.length() - 1);
+                prefix = prefix.substring(0, prefix.length()-1);
                 if (prefix.isEmpty()) {
                     return "";
                 }
