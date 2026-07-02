d1f352a3247ef874066fcc1623d309ceae000f2b
IshankAggarwal09 <aggarwalishu263@gmail.com>
2026-07-02 17:11:34 +0530

    Time: 0 ms (100%), Space: 46.5 MB (59.28%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.java
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -16,14 +16,13 @@
 class Solution {
     public int[] travel(TreeNode root) {
         if (root == null) {
-            return new int[2];
+            return new int[]{0, 0};
         }
         int[] leftNodeChoices = travel(root.left);
         int[] rightNodeChoices = travel(root.right);
-        int[] options = new int[2];
-        options[0] = root.val + leftNodeChoices[1] + rightNodeChoices[1];
-        options[1] = Math.max(leftNodeChoices[0], leftNodeChoices[1]) + Math.max(rightNodeChoices[0], rightNodeChoices[1]);
-        return options;
+        int rob = root.val + leftNodeChoices[1] + rightNodeChoices[1];
+        int notRob = Math.max(leftNodeChoices[0], leftNodeChoices[1]) + Math.max(rightNodeChoices[0], rightNodeChoices[1]);
+        return new int[]{rob, notRob};
     }
     public int rob(TreeNode root) {
         int[] options = travel(root);
