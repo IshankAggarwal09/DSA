693c807117edff6a7e7fa727f51d77a70c79c51d
IshankAggarwal09 <aggarwalishu263@gmail.com>
2026-07-12 15:51:12 +0530

    Time: 3 ms (87.86%), Space: 52.5 MB (33.71%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,47 +1,25 @@
-class Pair {
-    int first;
-    int second;
-    Pair(int first, int second) {
-        this.first = first;
-        this.second = second;
-    }
-}
-
 class Solution {
-    public void bfs(int ro, int co, int[][] vis, char[][] grid) {
-        vis[ro][co] = 1;
-        Queue<Pair> q = new LinkedList<>();
-        int m = grid.length;
-        int n = grid[0].length;
-        q.add(new Pair(ro, co));
-        int[] delRow = {-1, 0, 1, 0};
-        int[] delCol = {0, 1, 0, -1};
-        while (!q.isEmpty()) {
-            int row = q.peek().first;
-            int col = q.peek().second;
-            q.remove();
-            for (int k=0; k<4; k++) {
-                int nRow = row + delRow[k];
-                int nCol = col + delCol[k];
-                if (nRow >= 0 && nRow < m && nCol >= 0 && nCol < n && grid[nRow][nCol] == '1' && vis[nRow][nCol] == 0) {
-                    vis[nRow][nCol] = 1;
-                    q.add(new Pair(nRow, nCol));
-                }
-            }
+    public void dfs(char[][] grid, int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == '0') {
+            return;
         }
+        grid[row][col] = '0';
+        dfs(grid, row-1, col);
+        dfs(grid, row+1, col);
+        dfs(grid, row, col-1);
+        dfs(grid, row, col+1);
     }
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][] vis = new int[m][n];
         int count = 0;
         for (int i=0; i<m; i++) {
             for (int j=0; j<n; j++) {
-                if (vis[i][j] == 0 && grid[i][j] == '1') {
+                if (grid[i][j] == '1') {
                     count++;
-                    bfs(i, j, vis, grid);
+                    dfs(grid, i, j);
                 }
-            }   
+            }
         }
         return count;
     }
