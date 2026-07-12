be6f6651c7279bd2fac3b4a8bfe0e59237abd8b8
IshankAggarwal09 <aggarwalishu263@gmail.com>
2026-07-12 16:26:02 +0530

    Time: 0 ms (100%), Space: 46.9 MB (25.94%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,46 +1,20 @@
-class Pair {
-    int first;
-    int second;
-    Pair(int first, int second) {
-        this.first = first;
-        this.second = second;
-    }
-}
-
 class Solution {
-    public void bfs(int[][] image, int sr, int sc, int color, boolean[][] vis) {
-        vis[sr][sc] = true;
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(sr, sc));
-        int m = image.length;
-        int n = image[0].length;
-        int[] delRow = {-1, 0, 1, 0};
-        int[] delCol = {0, 1, 0, -1};
-        int ogColor = image[sr][sc];
-        image[sr][sc] = color;
-        while (!q.isEmpty()) {
-            int row = q.peek().first;
-            int col = q.peek().second;
-            q.remove();
-            for (int k=0; k<4; k++) {
-                int nRow = row + delRow[k];
-                int nCol = col + delCol[k];
-                if (nRow >= 0 && nRow < m && nCol >= 0 && nCol < n && image[nRow][nCol] == ogColor && vis[nRow][nCol] == false) {
-                    vis[nRow][nCol] = true;
-                    image[nRow][nCol] = color;
-                    q.add(new Pair(nRow, nCol));
-                }
-            }
+    public void dfs(int[][] image, int row, int col, int color, int original) {
+        if (row < 0 || row >= image.length || col < 0 || col >= image[0].length || image[row][col] != original || image[row][col] == color) {
+            return;
         }
+        image[row][col] = color;
+        dfs(image, row-1, col, color, original);
+        dfs(image, row+1, col, color, original);
+        dfs(image, row, col-1, color, original);
+        dfs(image, row, col+1, color, original);
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         if (image[sr][sc] == color) {
             return image;
         }
-        int m = image.length;
-        int n = image[0].length;
-        boolean[][] vis = new boolean[m][n];
-        bfs(image, sr, sc, color, vis);
+        int original = image[sr][sc];
+        dfs(image, sr, sc, color, original);
         return image;
     }
 }
