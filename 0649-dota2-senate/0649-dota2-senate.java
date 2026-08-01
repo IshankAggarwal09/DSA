a3fd4d8c65b04cdb215a5f6bfe841dae251eaa58
IshankAggarwal09 <aggarwalishu263@gmail.com>
2026-08-01 23:26:40 +0530

    Time: 12 ms (46.45%), Space: 47.2 MB (49.35%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String predictPartyVictory(String senate) {
+        Queue<Integer> r = new LinkedList<>();
+        Queue<Integer> d = new LinkedList<>();
+        int n = senate.length();
+        for (int i=0; i<n; i++) {
+            if (senate.charAt(i) == 'R') {
+                r.add(i);
+            } 
+            else {
+                d.add(i);
+            }
+        }
+        while (!r.isEmpty() && !d.isEmpty()) {
+            if (r.peek() < d.peek()) {
+                r.add(n++);
+            } 
+            else {
+                d.add(n++);
+            }
+            r.remove();
+            d.remove();
+        }
+        if (r.isEmpty()) {
+            return "Dire";
+        }
+        return "Radiant";
+    }
+}
