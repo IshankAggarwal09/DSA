81e192be52b9898025a3dedfb142caadeb1bb106
IshankAggarwal09 <aggarwalishu263@gmail.com>
2026-08-11 15:16:40 +0530

    Time: 21 ms (41.73%), Space: 45.9 MB (46.91%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -3,11 +3,11 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
         Arrays.sort(nums);
-        for (int i=0; i<n-3; i++) {
+        for (int i=0; i<n; i++) {
             if (i > 0 && nums[i] == nums[i-1]) {
                 continue;
             }
-            for (int j=i+1; j<n-2; j++) {
+            for (int j=i+1; j<n; j++) {
                 if (j != i+1 && nums[j] == nums[j-1]) {
                     continue;
                 }
