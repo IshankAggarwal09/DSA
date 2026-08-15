92a5d0edfdd9a88032cca803842afca1ca21f46e
IshankAggarwal09 <aggarwalishu263@gmail.com>
2026-08-15 22:44:07 +0530

    Time: 1 ms (96.17%), Space: 45.3 MB (43.18%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -3,16 +3,16 @@ class Solution {
         int n = nums.length;
         for (int i=0; i<n-1; i++) {
             if (nums[i] == nums[i+1]) {
-                nums[i] *= 2;
+                nums[i] = nums[i] * 2;
                 nums[i+1] = 0;
             }
         }
         int left = 0;
         for (int right=0; right<n; right++) {
             if (nums[right] != 0) {
-                int temp = nums[right];
-                nums[right] = nums[left];
-                nums[left] = temp;
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
                 left++;
             }
         }
